839cdf2dfea6835716d9e16e71b0c8c2f966a586
Mikhaillkin <ronaldovah@mail.ru>
2022-08-13 20:44:57 +0300

    ADD CategoryCreateController, Form catetgory

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController')->name('main.index');
 });
 
+
 Route::group(['namespace'=>'Admin', 'prefix'=>'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
-
     Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-        Route::get('/', 'IndexController');
+        Route::get('/', 'IndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
     });
 });
 
