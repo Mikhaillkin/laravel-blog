7143136fd2c680820239deadcdacc0c87cfaa11b
Mikhaillkin <ronaldovah@mail.ru>
2022-08-20 16:37:10 +0300

    Edited main.index view, edited Main/IndexController. Created Pagination,Popular posts,Related Posts on main.index page

--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
 
     public function __invoke()
     {
-//        return redirect()->route('post.index');
-        return view('main.index');
+        $posts = Post::paginate(6);
+        $randomPosts = Post::get()->random(4);
+        $popularPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
+//        dd($likedPosts);
+        return view('main.index',compact('posts', 'randomPosts', 'popularPosts'));
     }
 
 }
